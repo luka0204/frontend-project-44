27f9fc019b28c6a81cd976c8d99e48054aba242b
luka0204 <luka9609@gmail.com>
2023-12-13 11:34:18 +0300

    eighth commit

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/e9c5015d94bd1d2998f2/maintainability)](https://codeclimate.com/github/luka0204/frontend-project-44/maintainability)
 
-Task 4 asciinema: https://asciinema.org/a/BF8cDMI67IC62WOrs8uhgwAXg
+Task 5 asciinema: https://asciinema.org/a/BF8cDMI67IC62WOrs8uhgwAXg
 
-Task 5 asciinema https://asciinema.org/a/q93jmP7MIfux2LQKrEuJv7Pys
+Task 6 asciinema https://asciinema.org/a/q93jmP7MIfux2LQKrEuJv7Pys
 
-Task 6 aciinema https://asciinema.org/a/HiVEpofJbmqyI9gUJkuG51LOo
+Task 7 aciinema https://asciinema.org/a/HiVEpofJbmqyI9gUJkuG51LOo
+
+Task 8 asciinema https://asciinema.org/a/DvLjHFMC9pgqMvmvRPHBy9weV
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,23 +1,36 @@
-// import readlineSync from 'readline-sync';
-// import
-// { name } from '../src/cli.js';
-
-const randomNumberGen = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const rNum2 = randomNumberGen(1, 9);
-const rNum1 = randomNumberGen(50, 100);
-const arithmeticProgression = (n, lim) =>
-  // eslint-disable-next-line implicit-arrow-linebreak
-  Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
-const randomArray = (arithmeticProgression(rNum2, rNum1));
-console.log(`random array = ${randomArray}`);
-const lengthMin = (randomNumberGen(1, 4));
-const lengthMax = (lengthMin + 10);
-const slicedArray = randomArray.slice(lengthMin, lengthMax);
-console.log(`sliced array =  ${slicedArray}`);
-
-const randomNumInArray1 = (slicedArray.length - rNum2);
-const missingNumber = slicedArray[randomNumInArray1];
-console.log(missingNumber);
-const indexNum = slicedArray.indexOf(missingNumber);
-slicedArray[indexNum] = '...';
-console.log(`array with missing number = ${slicedArray}`);
+import readlineSync from 'readline-sync';
+import
+{ name } from '../src/cli.js';
+let point = 0;
+console.log('What number is missing in the progression?')
+for (let i = 0; i <= 2; i += 1) {
+  const randomNumberGen = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const rNum2 = randomNumberGen(1, 9);
+  const rNum3 = randomNumberGen(1,5);
+  const rNum1 = randomNumberGen(50, 100);
+  const arithmeticProgression = (n, lim) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
+  const randomArray = (arithmeticProgression(rNum2, rNum1));
+  const lengthMin = (randomNumberGen(1, 4));
+  const lengthMax = (lengthMin + 10);
+  const slicedArray = randomArray.slice(lengthMin, lengthMax);
+  const randomNumInArray1 = (slicedArray.length - rNum3);
+  const missingNumber = slicedArray[randomNumInArray1];
+  console.log(`это ответ чтобы самому не считать = ${missingNumber}`);
+  const indexNum = slicedArray.indexOf(missingNumber);
+  slicedArray[indexNum] = '...';
+  console.log(`Question: ${slicedArray}`);
+  let question = readlineSync.question('Your answer: ');
+  question = Number(question);
+  if (question === missingNumber) {
+    console.log('Correct!');
+    point += 1;
+  } else {
+console.log(`'${question}' is wrong answer ;(. Correct answer was '${missingNumber}'.`);
+break;
+  }
+  if (point === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
