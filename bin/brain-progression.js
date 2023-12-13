5ab7e72cff40308acf4131af3a6f0af0f57d45be
luka0204 <luka9609@gmail.com>
2023-12-13 11:37:11 +0300

    eighth commit

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,16 +1,17 @@
 import readlineSync from 'readline-sync';
 import
 { name } from '../src/cli.js';
+
 let point = 0;
-console.log('What number is missing in the progression?')
+console.log('What number is missing in the progression?');
 for (let i = 0; i <= 2; i += 1) {
   const randomNumberGen = (min, max) => Math.floor(Math.random() * (max - min)) + min;
   const rNum2 = randomNumberGen(1, 9);
-  const rNum3 = randomNumberGen(1,5);
+  const rNum3 = randomNumberGen(1, 5);
   const rNum1 = randomNumberGen(50, 100);
   const arithmeticProgression = (n, lim) =>
     // eslint-disable-next-line implicit-arrow-linebreak
-    Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);
+    Array.from({ length: Math.ceil(lim / n) }, (_, j) => (j + 1) * n);
   const randomArray = (arithmeticProgression(rNum2, rNum1));
   const lengthMin = (randomNumberGen(1, 4));
   const lengthMax = (lengthMin + 10);
@@ -27,8 +28,8 @@ for (let i = 0; i <= 2; i += 1) {
     console.log('Correct!');
     point += 1;
   } else {
-console.log(`'${question}' is wrong answer ;(. Correct answer was '${missingNumber}'.`);
-break;
+    console.log(`'${question}' is wrong answer ;(. Correct answer was '${missingNumber}'.`);
+    break;
   }
   if (point === 3) {
     console.log(`Congratulations, ${name}!`);
