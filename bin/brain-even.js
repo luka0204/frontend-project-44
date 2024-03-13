7c56ec30a0b262fd39bec7f62127941ea4d8056b
luka0204 <Luka9609@gmail.com>
2024-03-13 11:43:23 +0300

    brain-even done

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,22 +3,27 @@ import readlineSync from 'readline-sync';
 import
 // eslint-disable-next-line import/no-useless-path-segments
 { name } from '../src/cli.js';
-
+import questionModule from '../src/cli.js';
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let point = 0;
-for (let i = 0; i <= 2; i += 1) {
-  const rNum = Math.floor(Math.random() * 10);
-  console.log(`Question: ${rNum}`);
-  const question = readlineSync.question('Your answer: ');
-  if ((rNum % 2 === 0 && question.toLowerCase() === 'yes') || (rNum % 2 !== 0 && question.toLowerCase() === 'no')) {
-    console.log('Correct!');
-    point += 1;
-  } else {
-    console.log("yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${name}!`);
-    break;
+let point = 0
+for (let i = 0; i <= 2; i += 1){
+  function everOrOdd(num){
+    if (rNum % 2 === 0){
+      return 'yes';
+    } else {
+      return 'no';
+    }
+    }
+const rNum = Math.floor(Math.random() * 10);
+const answer = questionModule(rNum,everOrOdd(rNum))
+if (answer === true){
+  point += 1;
+} else {
+  break;
+}
   }
   if (point === 3) {
     console.log(`Congratulations, ${name}!`);
   }
-}
+
+
--- src/cli.js
+++ src/cli.js
@@ -4,3 +4,17 @@ console.log('Welcome to the Brain Games!');
 // eslint-disable-next-line import/prefer-default-export
 export const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
+
+export default function questionModule (answer, correctAnswer) {
+        console.log(`Question: ${answer}`);
+    const question = readlineSync.question('Your answer: ');
+    if(question === correctAnswer) {
+    console.log('Correct!');
+    return (true);
+    } else {
+    console.log(`${question} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    console.log(`Let's try again, ${name}!`);  
+    return (false);
+    }
+
+}
